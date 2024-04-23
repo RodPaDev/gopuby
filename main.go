7037505804e0ee57967502c60d5a75319b8e93c2
Patrick <47316946+RodPaDev@users.noreply.github.com>
2024-04-23 17:50:42 +0100

    add more ocmmented code

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "gopuby/epubManager"
+import "gopuby/screen"
+
+// import "gopuby/epubManager"
 
 func main() {
-	epubManager.Open("book.epub")
+	// epubManager.Open("book.epub")
+	// const path = "test.xhtml"
+	// const path = "books/tmp/6c088701-fa14-4dc6-b33e-c2749d99d647/EPUB/text/ch003.xhtml"
+	// str, err := htmlParse.ParseHtml(path)
+	screen := screen.New()
+	screen.StartResizeWatcher()
+
 }
