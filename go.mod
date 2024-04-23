c558838e68e3cb783055b6e9481338cefb77d327
Patrick <47316946+RodPaDev@users.noreply.github.com>
2024-04-23 20:20:30 +0100

    get text to render and scroll down

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.21.3
 require (
 	github.com/crazy3lf/colorconv v1.2.0
 	golang.org/x/net v0.24.0
-	golang.org/x/term v0.19.0
 )
 
-require golang.org/x/sys v0.19.0 // indirect
+require github.com/mattn/go-runewidth v0.0.9 // indirect
+
+require github.com/nsf/termbox-go v1.1.1
--- main.go
+++ main.go
@@ -1,15 +1,56 @@
 package main
 
-import "gopuby/screen"
+import (
+	"github.com/nsf/termbox-go"
+	"gopuby/htmlParse"
+	"gopuby/renderer"
+)
 
 // import "gopuby/epubManager"
 
 func main() {
 	// epubManager.Open("book.epub")
 	// const path = "test.xhtml"
-	// const path = "books/tmp/6c088701-fa14-4dc6-b33e-c2749d99d647/EPUB/text/ch003.xhtml"
-	// str, err := htmlParse.ParseHtml(path)
-	screen := screen.New()
-	screen.StartResizeWatcher()
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+	termbox.SetInputMode(termbox.InputEsc)
+	// Set the output mode, which determines how colors and attributes are used
+	termbox.SetOutputMode(termbox.OutputNormal)
+
+	// Clear the screen before writing
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	const path = "books/tmp/6c088701-fa14-4dc6-b33e-c2749d99d647/EPUB/text/ch003.xhtml"
+	str, err := htmlParse.ParseHtml(path)
+	if err != nil {
+		panic(err)
+	}
+
+	renderer := renderer.New()
+	renderer.Render(&str)
+
+eventLoop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Ch == 'q' {
+				break eventLoop
+			}
+			// check for arrow keys
+			switch ev.Key {
+			case termbox.KeyArrowUp:
+				renderer.ScrollUp(&str)
+			case termbox.KeyArrowDown:
+				renderer.ScrollDown(&str)
+			}
+
+		case termbox.EventResize:
+			renderer.Render(&str)
+		}
+	}
 
 }
