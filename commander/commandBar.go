b36c476608896846cb245be535d96ede9ab4c623
Patrick <47316946+RodPaDev@users.noreply.github.com>
2024-04-27 11:54:10 +0100

    handle cursor placement

--- commander/commandBar.go
+++ commander/commandBar.go
@@ -4,23 +4,8 @@ import (
 	"github.com/nsf/termbox-go"
 )
 
-// this is the command bar, it is the main entry point for all commands
-// when pressing space the command bar will be shown and the user can enter a command
-// the command bar will always be two lines height
-// the top most line will be the status bar
-// the bottom line will be the input line
-
-// the status line will show the current chapter and section
-// for now will only do the input line
-
-// to do the input line I use termbox to draw a line over the last row of the screen
-// and then I will draw a ">" character to show the user that he can enter a command
-// users press space to be to show the command bar and press space again to hide it
-
-// first lets draw the command bar with termbox
-// we go to the last row of the terminal and draw a top line
-
 func (c *Commander) DrawCommandBar() {
+
 	cols, rows := termbox.Size()
 	// draw the top line
 	for i := 0; i < cols; i++ {
@@ -33,6 +18,10 @@ func (c *Commander) DrawCommandBar() {
 		}
 		termbox.SetCell(i, rows-1, ' ', termbox.ColorWhite, termbox.ColorDefault)
 	}
+	termbox.Flush()
+
+	x, y := 2, rows-1
+	termbox.SetCursor(x+c.input.GetRuneCount(), y)
 	termbox.Flush()
 }
 
@@ -49,15 +38,12 @@ func (c *Commander) ClearCommandBar() {
 		termbox.SetCell(i, rows-2, ' ', termbox.ColorWhite, termbox.ColorDefault)
 	}
 	c.Renderer.Render(&c.ParsedText)
+	termbox.SetCursor(0, 0)
+	termbox.HideCursor()
 	termbox.Flush()
-
 }
 
-func (c *Commander) ToggleCommandBar() {
-	c.isOpen = !c.isOpen
-	if c.isOpen {
-		c.DrawCommandBar()
-	} else {
-		c.ClearCommandBar()
-	}
+func GetCommandBarInputPosition() (int, int) {
+	_, rows := termbox.Size()
+	return 0, rows - 1
 }
